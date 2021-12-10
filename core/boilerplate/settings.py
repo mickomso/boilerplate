1ac3e5c127d054881fccb01f4fc2fc9ff9c1c7b8
mickomso <mcompany@mercadona.es>
2021-12-10 19:47:38 +0100

    First example app created

--- core/boilerplate/settings.py
+++ core/boilerplate/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'boilerplate.example.apps.ExampleConfig',
 ]
 
 MIDDLEWARE = [
