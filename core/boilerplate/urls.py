3d3051915bcb10ebcda9389c6b031d3506c8caa7
mickomso <mcompany@mercadona.es>
2021-12-12 12:43:45 +0100

    Add homepage view and template

--- core/boilerplate/urls.py
+++ core/boilerplate/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from boilerplate.homepage.views import HomepageView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomepageView.as_view(), name='home'),
 ]
