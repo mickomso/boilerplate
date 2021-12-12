3ee09c59f290187dd529818669eef3fc639b16af
mickomso <mcompany@mercadona.es>
2021-12-12 12:48:26 +0100

    Add template variable

--- core/boilerplate/homepage/views.py
+++ core/boilerplate/homepage/views.py
@@ -3,3 +3,8 @@ from django.views.generic import TemplateView
 
 class HomepageView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['statement'] = 'Nice to see you!'
+        return context
